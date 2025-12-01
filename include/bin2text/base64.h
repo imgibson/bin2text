5762d04e707978e471ddaba2ef65c553aeee9a2d
imgibson <96395432+imgibson@users.noreply.github.com>
2025-12-01 11:11:04 +0900

    Remove exceptions and use a negative return value.

--- include/bin2text/base64.h
+++ include/bin2text/base64.h
@@ -10,80 +10,77 @@
 #pragma once
 
 #include <cassert>
+#include <cstddef>
 #include <cstdint>
 #include <cstring>
-#include <exception>
 #include <tuple>
 
 namespace base64 {
 
-enum class mode_t {
-    standard,
-    url_safe
-};
+enum class mode_t { standard, url_safe };
 
 template <mode_t mode = mode_t::standard>
-static std::size_t encoded_length(const void* buffer, std::size_t length) noexcept {
+static std::size_t encoded_length(const void* buffer, std::size_t size) noexcept {
 #ifdef _DEBUG
     assert(buffer);
 #endif
     std::ignore = buffer;
     if constexpr (mode == mode_t::standard) {
-        return (length + 2) / 3 * 4;
+        return (size + 2) / 3 * 4;
     } else {
-        std::size_t t = length % 3;
-        return (length / 3) * 4 + (t == 0 ? 0 : t + 1);
+        std::size_t t = size % 3;
+        return (size / 3) * 4 + (t == 0 ? 0 : t + 1);
     }
 }
 
 template <mode_t mode = mode_t::standard, std::size_t N>
-static std::size_t encode(const void* buffer, std::size_t length, char (&result)[N]) noexcept {
+static std::size_t encode(const void* buffer, std::size_t size, char (&result)[N]) noexcept
+    requires(N > 0) {
 #ifdef _DEBUG
     assert(buffer);
 #endif
-    static_assert(N > 0);
-    const char* kCharMap = []() consteval -> const char* {
+    const char* kCharSet = []() consteval -> const char* {
         if constexpr (mode == mode_t::standard) {
             return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/)";
         } else {
             return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_)";
         }
     }();
-    std::size_t size = encoded_length<mode>(buffer, length);
-    if (size >= N) {
-        return size;
+    std::size_t length = encoded_length<mode>(buffer, size);
+    if (length >= N) {
+        return length;
     }
-    size = 0;
+    length = 0;
     const char* bufptr = static_cast<const char*>(buffer);
-    while (length >= 3) {
-        result[size++] = kCharMap[(bufptr[0] & 0xfc) >> 2];
-        result[size++] = kCharMap[(bufptr[0] & 0x03) << 4 | (bufptr[1] & 0xf0) >> 4];
-        result[size++] = kCharMap[(bufptr[1] & 0x0f) << 2 | (bufptr[2] & 0xc0) >> 6];
-        result[size++] = kCharMap[(bufptr[2] & 0x3f)];
+    while (size >= 3) {
+        result[length++] = kCharSet[(bufptr[0] & 0xfc) >> 2];
+        result[length++] = kCharSet[(bufptr[0] & 0x03) << 4 | (bufptr[1] & 0xf0) >> 4];
+        result[length++] = kCharSet[(bufptr[1] & 0x0f) << 2 | (bufptr[2] & 0xc0) >> 6];
+        result[length++] = kCharSet[(bufptr[2] & 0x3f)];
         bufptr += 3;
-        length -= 3;
-    };
-    if (length >= 2) {
-        result[size++] = kCharMap[(bufptr[0] & 0xfc) >> 2];
-        result[size++] = kCharMap[(bufptr[0] & 0x03) << 4 | (bufptr[1] & 0xf0) >> 4];
-        result[size++] = kCharMap[(bufptr[1] & 0x0f) << 2];
+        size -= 3;
+    }
+    if (size >= 2) {
+        result[length++] = kCharSet[(bufptr[0] & 0xfc) >> 2];
+        result[length++] = kCharSet[(bufptr[0] & 0x03) << 4 | (bufptr[1] & 0xf0) >> 4];
+        result[length++] = kCharSet[(bufptr[1] & 0x0f) << 2];
         if constexpr (mode == mode_t::standard) {
-            result[size++] = '=';
+            result[length++] = '=';
         }
-    } else if (length >= 1) {
-        result[size++] = kCharMap[(bufptr[0] & 0xfc) >> 2];
-        result[size++] = kCharMap[(bufptr[0] & 0x03) << 4];
+    } else if (size >= 1) {
+        result[length++] = kCharSet[(bufptr[0] & 0xfc) >> 2];
+        result[length++] = kCharSet[(bufptr[0] & 0x03) << 4];
         if constexpr (mode == mode_t::standard) {
-            result[size++] = '=';
-            result[size++] = '=';
+            result[length++] = '=';
+            result[length++] = '=';
         }
     }
-    result[size] = '\0';
-    return size;
+    result[length] = '\0';
+    return length;
 }
 
 template <mode_t mode = mode_t::standard>
-static std::size_t decoded_length(const char* str, std::size_t length) {
+static std::ptrdiff_t decoded_length(const char* str, std::size_t length) noexcept {
 #ifdef _DEBUG
     assert(str);
 #endif
@@ -93,123 +90,113 @@ static std::size_t decoded_length(const char* str, std::size_t length) {
     std::size_t t = length % 4;
     if constexpr (mode == mode_t::standard) {
         if (t != 0) {
-            throw std::exception();
+            return -1;
         }
     } else {
         if (t == 1) {
-            throw std::exception();
+            return -1;
         }
     }
-    const auto is_valid = [](char c) -> bool {
-        const char* kCharMap = []() consteval -> const char* {
-            if constexpr (mode == mode_t::standard) {
-                return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/)";
-            } else {
-                return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_)";
-            }
-        }();
-        return std::strchr(kCharMap, c) != nullptr;
-    };
+    const char* kCharSet = []() consteval -> const char* {
+        if constexpr (mode == mode_t::standard) {
+            return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/)";
+        } else {
+            return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_)";
+        }
+    }();
     std::size_t i = 0;
-    while (i < length && is_valid(str[i])) {
+    while (i < length && std::strchr(kCharSet, str[i]) != nullptr) {
         ++i;
-    };
+    }
     if constexpr (mode == mode_t::standard) {
-        const auto is_trail = [](char c) -> bool {
-            return c == '=';
-        };
         std::size_t j = i;
-        while (j < length && is_trail(str[j])) {
+        while (j < length && str[j] == '=') {
             ++j;
-        };
+        }
         if (j != length || (j - i) > 2) {
-            throw std::exception();
+            return -1;
         }
         return length / 4 * 3 - (j - i);
     } else {
         if (i != length) {
-            throw std::exception();
+            return -1;
         }
         return length / 4 * 3 + (t == 0 ? 0 : t - 1);
     }
 }
 
-template <mode_t mode = mode_t::standard, std::size_t N>
-static std::size_t decode(const char* str, std::size_t length, char (&result)[N]) {
-#ifdef _DEBUG
-    assert(str);
-#endif
-    static_assert(N > 0);
-    const auto strmap = [](char c) -> char {
-        const char* kCharMap = []() consteval -> const char* {
-            if constexpr (mode == mode_t::standard) {
-                return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/)";
-            } else {
-                return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_)";
-            }
-        }();
-        const char* pos = std::strchr(kCharMap, c);
-        if (!pos) {
-            throw std::exception();
-        }
-        return static_cast<char>(pos - kCharMap);
-    };
-    std::size_t size = 0;
-    const auto append = [&](char c) -> void {
-        if (size >= N) {
-            throw std::exception();
-        }
-        result[size++] = c;
-    };
-    while (length > 4) {
-        char buffer[4];
-        for (std::size_t i = 0; i < 4; ++i) {
-            buffer[i] = strmap(*str++);
-        }
-        append(buffer[0] << 2 | buffer[1] >> 4);
-        append(buffer[1] << 4 | buffer[2] >> 2);
-        append(buffer[2] << 6 | buffer[3]);
-        length -= 4;
-    };
-    if (length >= 2) {
-        if constexpr (mode == mode_t::standard) {
-            while (length > 2 && str[length - 1] == '=') {
-                --length;
-            };
-        }
-        char buffer[4]{};
-        for (std::size_t i = 0; i < length; ++i) {
-            buffer[i] = strmap(*str++);
-        }
-        if (length >= 4) {
-            append(buffer[0] << 2 | buffer[1] >> 4);
-            append(buffer[1] << 4 | buffer[2] >> 2);
-            append(buffer[2] << 6 | buffer[3]);
-        } else if (length >= 3) {
-            append(buffer[0] << 2 | buffer[1] >> 4);
-            append(buffer[1] << 4 | buffer[2] >> 2);
-        } else {
-            append(buffer[0] << 2 | buffer[1] >> 4);
-        }
-    } else {
-        throw std::exception();
-    }
-    return size;
-}
-
 template <mode_t mode = mode_t::standard>
-static std::size_t decoded_length(const char* str) {
-#ifdef _DEBUG
-    assert(str);
-#endif
+static std::ptrdiff_t decoded_length(const char* str) noexcept {
     return decoded_length<mode>(str, std::strlen(str));
 }
 
 template <mode_t mode = mode_t::standard, std::size_t N>
-static std::size_t decode(const char* str, char (&result)[N]) {
+static std::ptrdiff_t decode(const char* str, std::size_t length, char (&result)[N]) noexcept
+    requires(N > 0) {
 #ifdef _DEBUG
     assert(str);
 #endif
+    const char* kCharSet = []() consteval -> const char* {
+        if constexpr (mode == mode_t::standard) {
+            return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/)";
+        } else {
+            return R"(ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_)";
+        }
+    }();
+    std::size_t size = 0;
+    const auto append = [&](char c) -> bool {
+        if (size >= N) {
+            return false;
+        }
+        result[size++] = c;
+        return true;
+    };
+    while (length > 4) {
+        char buffer[4]{};
+        for (std::size_t i = 0; i < 4; ++i) {
+            const char* pos = std::strchr(kCharSet, *str++);
+            if (!pos) {
+                return -1;
+            }
+            buffer[i] = static_cast<char>(pos - kCharSet);
+        }
+        if (!append(buffer[0] << 2 | buffer[1] >> 4)) return -1;
+        if (!append(buffer[1] << 4 | buffer[2] >> 2)) return -1;
+        if (!append(buffer[2] << 6 | buffer[3])) return -1;
+        length -= 4;
+    }
+    if (length >= 2) {
+        if constexpr (mode == mode_t::standard) {
+            while (length > 2 && str[length - 1] == '=') {
+                --length;
+            }
+        }
+        char buffer[4]{};
+        for (std::size_t i = 0; i < length; ++i) {
+            const char* pos = std::strchr(kCharSet, *str++);
+            if (!pos) {
+                return -1;
+            }
+            buffer[i] = static_cast<char>(pos - kCharSet);
+        }
+        if (length >= 4) {
+            if (!append(buffer[0] << 2 | buffer[1] >> 4)) return -1;
+            if (!append(buffer[1] << 4 | buffer[2] >> 2)) return -1;
+            if (!append(buffer[2] << 6 | buffer[3])) return -1;
+        } else if (length >= 3) {
+            if (!append(buffer[0] << 2 | buffer[1] >> 4)) return -1;
+            if (!append(buffer[1] << 4 | buffer[2] >> 2)) return -1;
+        } else {
+            if (!append(buffer[0] << 2 | buffer[1] >> 4)) return -1;
+        }
+    } else {
+        return -1;
+    }
+    return static_cast<std::ptrdiff_t>(size);
+}
+
+template <mode_t mode = mode_t::standard, std::size_t N>
+static std::ptrdiff_t decode(const char* str, char (&result)[N]) noexcept {
     return decode<mode>(str, std::strlen(str), result);
 }
 
